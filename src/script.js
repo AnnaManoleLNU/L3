0348bef8860059e30bfe52941766080d8fadeb66
Anna Manole <am224wd@student.lnu.se>
2023-10-12 13:49:05 +0200

    Add lib and configure vite

--- src/script.js
+++ src/script.js
@@ -1,18 +1,1 @@
-function uploadFile() {
-  const fileInput = document.getElementById('fileInput');
-  const file = fileInput.files[0];
-  const formData = new FormData();
-  formData.append('file', file);
-  
-  fetch('/api/upload', {
-      method: 'POST',
-      body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-      console.log('Success:', data);
-  })
-  .catch((error) => {
-      console.error('Error:', error);
-  });
-}
+console.log('Hello World!');
