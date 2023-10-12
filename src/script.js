d78aef0beced5fd3743c786d46de0e07be17ed87
Anna Manole <am224wd@student.lnu.se>
2023-10-12 15:49:42 +0200

    CSS and first functionality

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+const usernamePrompt = document.getElementById('username-prompt');
+const usernameInput = document.getElementById('username');
+const usernameForm = document.getElementById('username-form');
+const gameDisplay = document.getElementById('game-display');
+
+// Add an event listener to the form
+usernameForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting
+  const username = usernameInput.value;
+  // Save the username in local storage
+  localStorage.setItem('username', username);
+  // hide the username input and button
+  usernamePrompt.classList.add('hidden');
+  usernameInput.classList.add('hidden');
+  usernameForm.classList.add('hidden');
+  // show the game display
+  gameDisplay.classList.remove('hidden');
+});
