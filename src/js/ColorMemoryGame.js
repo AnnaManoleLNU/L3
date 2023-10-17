239dc984b4af495bb9a0102c079f52628d6ad7d0
Anna Manole <am224wd@student.lnu.se>
2023-10-17 14:26:19 +0200

    Fix bug in win game condition

--- src/js/ColorMemoryGame.js
+++ src/js/ColorMemoryGame.js
@@ -1,22 +1,23 @@
-import { ColorGenerator } from "../../lib/js/ColorGenerator.js";
-import { ColorSchemeGenerator } from "../../lib/js/ColorSchemeGenerator.js";
+import { ColorGenerator } from '../../lib/js/ColorGenerator.js'
+import { ColorSchemeGenerator } from '../../lib/js/ColorSchemeGenerator.js'
+
 
 export class ColorMemoryGame {
-  #startScreen;
-  #difficultyScreen;
-  #gameScreen;
-  #gameBoard;
-  #endScreen;
-  #originalTiles = [];
-  #shuffledTiles = [];
-  #userTiles = [];
+  #startScreen
+  #difficultyScreen
+  #gameScreen
+  #gameBoard
+  #endScreen
+  #originalTiles = []
+  #shuffledTiles = []
+  #userTiles = []
 
   constructor() {
-    this.#startScreen = document.getElementById("start-screen");
-    this.#difficultyScreen = document.getElementById("difficulty-screen");
-    this.#gameScreen = document.getElementById("game-screen");
-    this.#gameBoard = document.getElementById("game-board");
-    this.#endScreen = document.getElementById("end-screen");
+    this.#startScreen = document.getElementById('start-screen');
+    this.#difficultyScreen = document.getElementById('difficulty-screen');
+    this.#gameScreen = document.getElementById('game-screen');
+    this.#gameBoard = document.getElementById('game-board');
+    this.#endScreen = document.getElementById('end-screen');
   }
 
   initiateGame() {
@@ -27,42 +28,42 @@ export class ColorMemoryGame {
   }
 
   #startGame() {
-    const usernameForm = document.getElementById("username-form");
-    const usernameInput = document.getElementById("username-input");
-    usernameForm.addEventListener("submit", (event) => {
+    const usernameInput = document.getElementById('username-input');
+    const usernameForm = document.getElementById('username-form');
+    usernameForm.addEventListener('submit', (event) => {
       event.preventDefault();
-      this.#startScreen.classList.add("hidden");
-      this.#difficultyScreen.classList.remove("hidden");
-      localStorage.setItem("username", usernameInput.value);
-      usernameInput.value = "";
+      this.#startScreen.classList.add('hidden');
+      this.#difficultyScreen.classList.remove('hidden');
+      localStorage.setItem('username', usernameInput.value);
+      usernameInput.value = '';
     });
   }
 
   #selectDifficulty() {
-    this.#difficultyScreen.addEventListener("click", (event) => {
+    this.#difficultyScreen.addEventListener('click', (event) => {
       const difficulty = event.target.id;
-      if (difficulty === "easy") {
+      if (difficulty === 'easy') {
         this.#setupDifficultyWithNumberOfTiles(3);
       }
-      if (difficulty === "medium") {
+      if (difficulty === 'medium') {
         this.#setupDifficultyWithNumberOfTiles(4);
       }
-      if (difficulty === "hard") {
+      if (difficulty === 'hard') {
         this.#setupDifficultyWithNumberOfTiles(5);
       }
     });
   }
 
   #setupDifficultyWithNumberOfTiles(numberOfTiles) {
-    this.#difficultyScreen.classList.add("hidden");
-    this.#gameScreen.classList.remove("hidden");
+    this.#difficultyScreen.classList.add('hidden');
+    this.#gameScreen.classList.remove('hidden');
     this.#generateTiles(numberOfTiles);
     this.#initiateGuessingGame();
   }
 
   #generateTiles(numberOfTiles) {
-    const colorsForTiles = [];
-    const numberOfColorsInScheme = 3;
+    const colorsForTiles = []
+    const numberOfColorsInScheme = 3
 
     for (let i = 0; i < numberOfTiles / numberOfColorsInScheme; i++) {
       const generatedColors = this.#generateColors();
@@ -72,9 +73,9 @@ export class ColorMemoryGame {
     }
 
     for (let i = 0; i < numberOfTiles; i++) {
-      const tile = document.createElement("button");
-      tile.classList.add("tile");
-      tile.classList.add("disabled");
+      const tile = document.createElement('button');
+      tile.classList.add('tile');
+      tile.classList.add('disabled');
       tile.style.backgroundColor = colorsForTiles[i];
       this.#originalTiles.push(tile);
     }
@@ -91,13 +92,13 @@ export class ColorMemoryGame {
     colorScheme.push(color);
 
     const colorSchemeGenerator = new ColorSchemeGenerator();
-    const generatedColors =
-      colorSchemeGenerator.generateAnalogousColorScheme(color);
+    const generatedColors = colorSchemeGenerator.generateAnalogousColorScheme(color);
     for (let i = 0; i < generatedColors.length; i++) {
       colorScheme.push(generatedColors[i]);
     }
     return colorScheme;
   }
+
 
   #initiateGuessingGame() {
     setTimeout(() => {
@@ -106,16 +107,16 @@ export class ColorMemoryGame {
         this.#gameBoard.removeChild(this.#gameBoard.firstChild);
       }
       // Reconstructing and appending shuffled tiles
-      this.#shuffledTiles.forEach((tile) => {
-        const newTile = document.createElement("button");
-        newTile.classList.add("tile");
+      this.#shuffledTiles.forEach(tile => {
+        const newTile = document.createElement('button');
+        newTile.classList.add('tile');
         newTile.style.backgroundColor = tile.style.backgroundColor;
         this.#gameBoard.appendChild(newTile);
       });
 
-      const gameTitle = document.querySelector("#game-screen h2");
-      gameTitle.textContent = "Tiles shuffled!";
-      document.getElementById("user-guesses").classList.remove("hidden");
+      const gameTitle = document.querySelector('#game-screen h2');
+      gameTitle.textContent = 'Tiles shuffled!';
+      document.getElementById('user-guesses').classList.remove('hidden');
 
       this.#tileOnClick();
     }, 4000);
@@ -125,21 +126,18 @@ export class ColorMemoryGame {
     this.#shuffledTiles = Array.from(this.#originalTiles);
     for (let i = this.#shuffledTiles.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [this.#shuffledTiles[i], this.#shuffledTiles[j]] = [
-        this.#shuffledTiles[j],
-        this.#shuffledTiles[i],
-      ];
+      [this.#shuffledTiles[i], this.#shuffledTiles[j]] = [this.#shuffledTiles[j], this.#shuffledTiles[i]];
     }
     return this.#shuffledTiles;
   }
 
   #tileOnClick() {
-    document.querySelectorAll("#game-board .tile").forEach((tile) => {
-      tile.addEventListener("click", () => {
-        const userTile = document.createElement("button");
-        userTile.classList.add("tile");
+    document.querySelectorAll('#game-board .tile').forEach(tile => {
+      tile.addEventListener('click', () => {
+        const userTile = document.createElement('button');
+        userTile.classList.add('tile');
         userTile.style.backgroundColor = tile.style.backgroundColor;
-        document.getElementById("guesses").appendChild(userTile);
+        document.getElementById('guesses').appendChild(userTile);
         this.#userTiles.push(userTile);
       });
     });
@@ -150,9 +148,11 @@ export class ColorMemoryGame {
       return false;
     }
     for (let i = 0; i < this.#originalTiles.length; i++) {
-      this.#isTileColorCorrect(i);
-      return true;
+      if (!this.#isTileColorCorrect(i)) {
+        return false;
+      }
     }
+    return true;
   }
 
   #isTileLengthCorrect() {
@@ -164,10 +164,7 @@ export class ColorMemoryGame {
   }
 
   #isTileColorCorrect(i) {
-    if (
-      this.#originalTiles[i].style.backgroundColor ===
-      this.#userTiles[i].style.backgroundColor
-    ) {
+    if (this.#originalTiles[i].style.backgroundColor === this.#userTiles[i].style.backgroundColor) {
       return true;
     } else {
       return false;
@@ -175,17 +172,17 @@ export class ColorMemoryGame {
   }
 
   #endGame() {
-    const submitGameButton = document.getElementById("game-submit");
-    submitGameButton.addEventListener("click", () => {
-      this.#gameScreen.classList.add("hidden");
-      this.#endScreen.classList.remove("hidden");
+    const submitGameButton = document.getElementById('game-submit');
+    submitGameButton.addEventListener('click', () => {
+      this.#gameScreen.classList.add('hidden');
+      this.#endScreen.classList.remove('hidden');
       this.#displayEndMessage();
     });
   }
 
   #displayEndMessage() {
-    const endMessage = document.querySelector("#end-screen h2");
-    const username = localStorage.getItem("username");
+    const endMessage = document.querySelector('#end-screen h2');
+    const username = localStorage.getItem('username');
     if (this.#isGameWon()) {
       endMessage.textContent = `Congratulations ${username}! You won!`;
     } else {
@@ -194,13 +191,13 @@ export class ColorMemoryGame {
   }
 
   #playAgain() {
-    const playAgainButton = document.getElementById("play-again");
-    playAgainButton.addEventListener("click", () => {
+    const playAgainButton = document.getElementById('play-again');
+    playAgainButton.addEventListener('click', () => {
       this.#clearTiles();
       this.#removeUserGuesses();
       this.#resetGuessingGame();
-      this.#endScreen.classList.add("hidden");
-      this.#startScreen.classList.remove("hidden");
+      this.#endScreen.classList.add('hidden');
+      this.#startScreen.classList.remove('hidden');
     });
   }
 
@@ -212,16 +209,16 @@ export class ColorMemoryGame {
   }
 
   #removeUserGuesses() {
-    const userTiles = document.getElementById("guesses");
+    const userTiles = document.getElementById('guesses');
     while (userTiles.firstChild) {
       userTiles.removeChild(userTiles.firstChild);
     }
-    this.#userTiles = [];
+    this.#userTiles = []
   }
 
   #resetGuessingGame() {
-    const gameTitle = document.querySelector("#game-screen h2");
-    gameTitle.textContent = "Shuffling tiles...";
-    document.getElementById("user-guesses").classList.add("hidden");
+    const gameTitle = document.querySelector('#game-screen h2');
+    gameTitle.textContent = 'Shuffling tiles...';
+    document.getElementById('user-guesses').classList.add('hidden');
   }
 }
