ef745a15ece66f0d6c60a930e6ebac1840e42576
Anna Manole <am224wd@student.lnu.se>
2023-10-17 18:43:58 +0200

    Update css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import { ColorMemoryGame } from './js/ColorMemoryGame.js';
-import { ThemeSwitcher } from './js/ThemeSwitcher.js';
 
 const colorMemoryGame = new ColorMemoryGame();
 colorMemoryGame.initiateGame();
-
-// const themeSwitcher = new ThemeSwitcher();
-// themeSwitcher.initiateThemeSwitcher();
